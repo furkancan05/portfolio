d1e27b6889ed21f33639f251ce40198f94e0adb5
furkancan05 <furkankulis@hotmail.com>
2024-08-26 11:48:40 +0300

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import "./globals.css";
 
@@ -39,6 +40,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <SmoothScrolling>{children}</SmoothScrolling>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
